802492cce36978a5f7a949771dd97d58ffe8790b
Selami <s.cetinguney@corlidogroup.com>
2022-01-13 21:19:53 +0100

    integration with plug and custom amount field

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TextField } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -7,9 +8,11 @@ import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import { createTheme, responsiveFontSizes, styled, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 function App() {
+	const [customAmount, setCustomAmount] = useState(0);
+
 	const theme = useMemo(() => {
 		let theme = createTheme({
 			palette: {
@@ -36,13 +39,40 @@ function App() {
 
 		return theme;
 	}, []);
+
+	const handleOnDonate = (amount: number) => {
+		try {
+			(window as any).ic.plug.isConnected().then((connected: any) => {
+				if (connected) {
+					(window as any).ic.plug.requestTransfer({
+						to: 'rszt5-dlas5-k7rk5-tdtb2-iqpsh-5tf7q-hfpoc-led3k-xgdqn-shcav-uae',
+						amount
+					});
+				} else {
+					(window as any).ic.plug.requestConnect().then((response: any) => {
+						if (response) {
+							(window as any).ic.plug.requestTransfer({
+								to: 'rszt5-dlas5-k7rk5-tdtb2-iqpsh-5tf7q-hfpoc-led3k-xgdqn-shcav-uae',
+								amount
+							});
+						}
+					});
+				}
+			});
+		} catch (error) {
+			console.error(error);
+			alert('something went wrong when initializing PLUG, do you have the extension installed?');
+		}
+	};
+
 	return (
 		<ThemeProvider theme={theme}>
 			<Container
-				disableGutters
+				// disableGutters
 				fixed
 				sx={{
 					marginTop: 2,
+					marginBottom: 2,
 					display: 'flex',
 					justifyContent: 'center',
 					flexDirection: 'column',
@@ -51,7 +81,7 @@ function App() {
 			>
 				<Stack spacing={2} direction='column' justifyContent='center' alignItems='center'>
 					<Avatar alt='Lukas' src='/lukas.jpg' sx={{ width: 200, height: 200 }} />
-					<Typography variant='h4' color='text.primary' fontFamily='Luxurious Roman'>
+					<Typography variant='h4' color='text.primary' textAlign='center' fontFamily='Luxurious Roman'>
 						Hi, I am Lukas and this is my story!
 					</Typography>
 					<Divider
@@ -66,7 +96,7 @@ function App() {
 							marginTop: `0 !important`
 						}}
 					>
-						<Grid item xs={6}>
+						<Grid item xs={12} sm={12} md={6}>
 							<Box
 								sx={{
 									color: 'text.secondary'
@@ -88,7 +118,7 @@ function App() {
 								</Typography>
 							</Box>
 						</Grid>
-						<Grid item xs={6}>
+						<Grid item xs={12} sm={12} md={6}>
 							<Typography
 								variant='h5'
 								textAlign='center'
@@ -101,36 +131,73 @@ function App() {
 							</Typography>
 							<Grid container spacing={1}>
 								<Grid item xs={4}>
-									<StyledButton variant='outlined' size='large'>
+									<StyledButton
+										variant='outlined'
+										size='large'
+										onClick={() => handleOnDonate(100_000_000)}
+									>
 										1 ICP
 									</StyledButton>
 								</Grid>
 								<Grid item xs={4}>
-									<StyledButton variant='outlined' size='large'>
+									<StyledButton
+										variant='outlined'
+										size='large'
+										onClick={() => handleOnDonate(500_000_000)}
+									>
 										5 ICP
 									</StyledButton>
 								</Grid>
 								<Grid item xs={4}>
-									<StyledButton variant='outlined' size='large'>
+									<StyledButton
+										variant='outlined'
+										size='large'
+										onClick={() => handleOnDonate(1_000_000_000)}
+									>
 										10 ICP
 									</StyledButton>
 								</Grid>
 								<Grid item xs={6}>
-									<StyledButton variant='outlined' size='large'>
+									<StyledButton
+										variant='outlined'
+										size='large'
+										onClick={() => handleOnDonate(1_500_000_000)}
+									>
 										15 ICP
 									</StyledButton>
 								</Grid>
 								<Grid item xs={6}>
-									<StyledButton variant='outlined' size='large'>
+									<StyledButton
+										variant='outlined'
+										size='large'
+										onClick={() => handleOnDonate(2_000_000_000)}
+									>
 										20 ICP
 									</StyledButton>
 								</Grid>
-								<Grid item xs={12}>
-									<StyledButton variant='contained' size='large'>
-										Donate custom amount
-									</StyledButton>
-								</Grid>
 							</Grid>
+							<Divider
+								sx={{
+									width: '100%',
+									marginY: 2
+								}}
+							/>
+							<Box display='flex' flexDirection={['column', 'column', 'row']}>
+								<StyledTextField
+									label='Custom amount'
+									type='number'
+									onChange={e => setCustomAmount(Number(e.target.value))}
+								/>
+								<Box marginLeft={[0, 0, 1]} marginTop={[1, 1, 0]} width={['100%', '100%', '50%']}>
+									<StyledButton
+										variant='contained'
+										size='large'
+										onClick={() => handleOnDonate(customAmount * 100_000_000)}
+									>
+										Donate
+									</StyledButton>
+								</Box>
+							</Box>
 						</Grid>
 					</Grid>
 				</Stack>
@@ -146,6 +213,17 @@ const StyledButton = styled(Button)(({ theme }) => ({
 	borderRadius: 500,
 	width: '100%',
 	textTransform: 'initial'
+}));
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+	'& .MuiInputBase-root': {
+		borderRadius: 50,
+		color: 'black',
+		paddingLeft: theme.spacing(0.5)
+	},
+	'& .MuiInputLabel-root': {
+		paddingLeft: theme.spacing(0.5)
+	}
 }));
 
 export default App;
