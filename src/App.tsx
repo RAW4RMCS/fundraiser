ae7932c49218ca7a8f3ef56ff09fb0ce5367f533
RAW4RMCS <rmc.sprenkels@gmail.com>
2022-01-31 22:10:39 +0100

    Add video and texts

--- src/App.tsx
+++ src/App.tsx
@@ -45,14 +45,14 @@ function App() {
 			(window as any).ic.plug.isConnected().then((connected: any) => {
 				if (connected) {
 					(window as any).ic.plug.requestTransfer({
-						to: 'rszt5-dlas5-k7rk5-tdtb2-iqpsh-5tf7q-hfpoc-led3k-xgdqn-shcav-uae',
+						to: '8ead44e70d740aa347203143b3d8ab09c5aeed318aeb3db2e527846875c27e2a',
 						amount
 					});
 				} else {
 					(window as any).ic.plug.requestConnect().then((response: any) => {
 						if (response) {
 							(window as any).ic.plug.requestTransfer({
-								to: 'rszt5-dlas5-k7rk5-tdtb2-iqpsh-5tf7q-hfpoc-led3k-xgdqn-shcav-uae',
+								to: '8ead44e70d740aa347203143b3d8ab09c5aeed318aeb3db2e527846875c27e2a',
 								amount
 							});
 						}
@@ -97,37 +97,65 @@ function App() {
 						}}
 					>
 						<Grid item xs={12} sm={12} md={6}>
-							<Box
-								sx={{
-									color: 'text.secondary'
-								}}
-							>
-								<Typography variant='body1' textAlign='justify' gutterBottom>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget pretium
-									augue, eu commodo lectus. Phasellus at feugiat massa. In at egestas arcu. Vivamus
-									volutpat turpis sit amet sagittis efficitur. Sed at finibus sapien. Maecenas id nunc
-									ac nisl pharetra efficitur quis vel est. Donec sit amet quam molestie, volutpat arcu
-									at, dictum nulla. Suspendisse congue vehicula aliquam.
-								</Typography>
-								<Typography variant='body1' textAlign='justify'>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget pretium
-									augue, eu commodo lectus. Phasellus at feugiat massa. In at egestas arcu. Vivamus
-									volutpat turpis sit amet sagittis efficitur. Sed at finibus sapien. Maecenas id nunc
-									ac nisl pharetra efficitur quis vel est. Donec sit amet quam molestie, volutpat arcu
-									at, dictum nulla. Suspendisse congue vehicula aliquam.
-								</Typography>
+							<Box display='flex' justifyContent='center' marginBottom={1}>
+								<video style={{borderRadius: 20}} width='360' height='640' controls>
+									<source src='lukas_explain.mov' type='video/mp4' />
+									Your browser does not support the video tag.
+								</video>
 							</Box>
+							<Typography
+								variant='caption'
+								color='text.secondary'
+								fontFamily='Luxurious Roman'
+							>
+								We asked him to make this video was made short after his diagnose (19 nov. 2021)
+							</Typography>
 						</Grid>
 						<Grid item xs={12} sm={12} md={6}>
 							<Typography
-								variant='h5'
-								textAlign='center'
+								variant='body2'
 								gutterBottom
 								color='text.secondary'
-								fontWeight='bold'
 								fontFamily='Luxurious Roman'
 							>
-								Fundraiser
+								Hello everyone, I’m Martin, a team member of NFT Studio. As you might have heard, our
+								founder Lukas has been diagnosed with cancer. To provide for his family, me and a couple
+								of friend and co-internet computer developers decided to set up a fundraiser.
+							</Typography>
+							<Divider style={{ margin: '16px' }} />
+							<Typography gutterBottom variant='h6'>
+								Who is Lukas Merville?
+							</Typography>
+							<Typography
+								variant='body2'
+								gutterBottom
+								style={{ paddingBottom: '16px' }}
+								color='text.secondary'
+								fontFamily='Luxurious Roman'
+							>
+								Lukas graduated in Art History, Philosophy of Arts and Literature. After graduation he
+								decided to explore other countries, he lived in Japan and South Korea for ten years.
+								During that time, he met his girlfriend Anna and since then they are in love, living in
+								Poland. Lukas is a smart guy who made attention of big tech companies and people in the
+								Crypto sphere.
+							</Typography>
+							<Typography gutterBottom variant='h6'>
+								What happened?
+							</Typography>
+							<Typography
+								variant='body2'
+								gutterBottom
+								style={{ paddingBottom: '16px' }}
+								color='text.secondary'
+								fontFamily='Luxurious Roman'
+							>
+								As some of you know he founded the project called NFT Studio approximately 6 months ago
+								while he was living in Poland. Lukas was developing his project when he and his
+								girlfriend found out that she was pregnant. The happiness did not last long
+								because Lukas was diagnosed with colon cancer. He started treatment in
+								Poland, but the doctors recommend him to continue his treatment in Canada. The situation is
+								difficult, and Lukas keeps fighting cancer. He is not able to work right now and his
+								pregnant girlfriend neither, so they do not have a stable income.
 							</Typography>
 							<Grid container spacing={1}>
 								<Grid item xs={4}>
